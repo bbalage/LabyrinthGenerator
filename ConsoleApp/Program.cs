d721ad10001a99bd34958348bd305847ae404f93
Bolyki Balázs <bbalage0@gmail.com>
2023-09-19 21:37:55 +0200

    Added docs.
    (and some code and comments to Program.cs)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,24 +15,34 @@ namespace ConsoleApp
             colorDict.Add(RoomType.SAWMILL, (new Color { R = 255, G = 100, B = 100 }, new Color { R = 255, G = 150, B = 150 }));
             colorDict.Add(RoomType.HOSPITAL, (new Color { R = 255, G = 255, B = 255 }, new Color { R = 255, G = 255, B = 255 }));
             LBuilder builder = new ColoredLBuilder(colorDict);
-            Labyrinth lab = builder.PushConnectingStrategy(new StraightCorridorConnectingStrategy())
-                .AddRoom("Room1", 0, 0, 50, 50, RoomType.FACTORY)
-                .AddRoom("Room2", 100, 0, 100, 100, RoomType.SAWMILL)
-                .AddRoom("Room3", 0, -200, 50, 100, RoomType.SAWMILL)
-                .AddRoom("Room4", 210, 25, 200, 50, RoomType.HOSPITAL)
-                .AddRoom("Room5", 100, -150, 100, 50, RoomType.FACTORY)
+            ConnectingStrategy connectingStrategy = new StraightCorridorConnectingStrategy();
+            // ConnectingStrategy connectingStrategy = new TouchingConnectingStrategy();
+            //Labyrinth lab = builder.PushConnectingStrategy(connectingStrategy)
+            //    .AddRoom("Room1", 0, 0, 50, 50, RoomType.FACTORY)
+            //    .AddRoom("Room2", 100, 0, 100, 100, RoomType.SAWMILL)
+            //    .AddRoom("Room3", 0, -200, 50, 100, RoomType.SAWMILL)
+            //    .AddRoom("Room4", 210, 25, 200, 50, RoomType.HOSPITAL)
+            //    .AddRoom("Room5", 100, -150, 100, 50, RoomType.FACTORY)
+            //    .Connect("Room1", "Room2")
+            //    .Connect("Room1", "Room3")
+            //    .Connect("Room2", "Room4")
+            //    .Connect("Room3", "Room5")
+
+            //    .Connect("Room2", "Room5")
+            //    .Build();
+            Labyrinth lab = builder.PushConnectingStrategy(connectingStrategy)
+                .AddRoom("Room1", 0, 0, 25, 25, RoomType.FACTORY)
+                .AddRoom("Room2", 40, 0, 10, 20, RoomType.SAWMILL)
+                .AddRoom("Room3", 0, -30, 30, 15, RoomType.SAWMILL)
                 .Connect("Room1", "Room2")
                 .Connect("Room1", "Room3")
-                .Connect("Room2", "Room4")
-                .Connect("Room3", "Room5")
-                .Connect("Room2", "Room5")
                 .Build();
-            //LIterator iterator = new AllLIterator(lab);
-            LIterator iterator = new NeighborLIterator(lab, new Vec2 { X = 50, Y = 50});
+            LIterator iterator = new AllLIterator(lab);
+            // LIterator iterator = new NeighborLIterator(lab, new Vec2 { X = 50, Y = 50});
             LabyrinthDrawer drawer = new BitmapLDrawer(iterator);
-            // LabyrinthDrawer drawer2 = new CharacterLDrawer(iterator);
+            LabyrinthDrawer drawer2 = new CharacterLDrawer(iterator);
             drawer.Draw(lab);
-            // drawer2.Draw(lab);
+            drawer2.Draw(lab);
         }
     }
 }
